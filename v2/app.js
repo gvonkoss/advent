dc594a5275a3d5e6421fe420b89cfb15828f1570
Roi Driscoll <roi@heytempo.com>
2019-12-22 13:16:27 +0000

    DAYumn that's a lotta days...

--- v2/app.js
+++ v2/app.js
@@ -1,1 +1,21 @@
 // express middleware lives here
+const express = require('express')
+const nunjucks = require('nunjucks');
+const app = express();
+const port = 8080;
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/', function (req, res) {
+  res.render('index.html', {username: 'GABI'});
+});
+
+app.get('/days/:dayNumber', function (req, res) {
+  const { dayNumber } = req.params;
+  res.render('days.html', { dayNumber: dayNumber });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
